8d6e89ef08c616ab36b8becacb073a8445fb9f41
hakan.fikret.senturk <hakan.fikret.senturk@gmail.com>
2021-02-12 19:10:24 +0300

    Proje dosyası ekle.

---
+++ Businness/Concrete/CarManager.cs
@@ -0,0 +1,21 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Businness.Concrete
+{
+    public class CarManager : ICarService
+    {
+        ICarDal _carDal;
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+        public List<Car> GetAll()
+        {
+            return _carDal.GetAll();
+        }
+    }
+}
---
+++ ReCapProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using Businness.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ReCapProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+
+            foreach (var cars in carManager.GetAll())
+            {
+                Console.WriteLine(cars.CarName+" "+cars.Description);
+            }
+        }
+
+        
+    }
+}
